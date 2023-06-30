e9a8be922ba3c0c28429bd48e5fc11d629515490
Cyril <c.public@tutanota.com>
2023-06-30 23:57:39 +0200

    setup project to use drop-down-picker's custom theme

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
+import { useState } from "react";
+import myTheme from "./myTheme";
 
 export default function App() {
+  DropDownPicker.addTheme("MyThemeName", myTheme);
+  DropDownPicker.setTheme("MyThemeName");
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    { label: "Apple", value: "apple" },
+    { label: "Banana", value: "banana" },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 10,
   },
 });
